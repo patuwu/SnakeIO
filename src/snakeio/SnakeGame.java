49a0d8634181d68ca4a15e6f4d42172e877eef26
patuwu <75994302+patuwu@users.noreply.github.com>
2021-12-13 11:30:53 +0800

    update1
    main menu

--- src/snakeio/SnakeGame.java
+++ src/snakeio/SnakeGame.java
@@ -12,13 +12,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class SnakeGame extends JPanel implements ActionListener {
+public class SnakeGame extends JPanel /*implements ActionListener*/ {
 
-    int WindowW = 300;
-    int WindowH = 300;
+    int WindowW = 400;
+    int WindowH = 400;
     int SquareSize = 10;
     int AllSquare = 900;
     int RAND_POS = 29;
@@ -43,6 +45,10 @@ public class SnakeGame extends JPanel implements ActionListener {
     Image headE;
     Image body;
     Image bodyE;
+    
+    JButton execServer = new JButton();;
+    JButton execClient = new JButton();;
+    JLabel title;
 
     public SnakeGame() {
         
@@ -68,13 +74,53 @@ public class SnakeGame extends JPanel implements ActionListener {
     }
     
     public void initBoard() {
-        addKeyListener(new TAdapter());
         setBackground(Color.black);
         setFocusable(true);
+        setLayout(null);
 
-        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+        setPreferredSize(new Dimension(WindowW, WindowH));
         loadImages();
-        initGame();
+        initWindow();
+    }
+    
+    public void initWindow() {
+        switch(gameState){
+            case 0:
+                
+            title = new JLabel("MULTI SNAKE");
+            title.setFont(new java.awt.Font("Segoe UI", 0, 24));
+            title.setForeground(new java.awt.Color(255, 255, 255));
+            title.setBounds(125, 50, 150, 40);
+            
+            execServer.setBackground(new java.awt.Color(0, 0, 0));
+            execServer.setFont(new java.awt.Font("Segoe UI Black", 1, 12)); // NOI18N
+            execServer.setForeground(new java.awt.Color(255, 255, 255));
+            execServer.setText("START SERVER");
+            execServer.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 2, true));
+            execServer.setContentAreaFilled(false);
+            execServer.setFocusPainted(false);
+            /*execServer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                initServer();
+                }
+            });*/
+            execServer.setBounds(140, 160, 120, 50);
+            
+            execClient.setBackground(new java.awt.Color(0, 0, 0));
+            execClient.setFont(new java.awt.Font("Segoe UI Black", 1, 12)); // NOI18N
+            execClient.setForeground(new java.awt.Color(255, 255, 255));
+            execClient.setText("JOIN");
+            execClient.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 2, true));
+            execClient.setContentAreaFilled(false);
+            execClient.setFocusPainted(false);
+            execClient.setBounds(140, 250, 120, 50);
+            
+            
+            this.add(title);
+            this.add(execServer);
+            this.add(execClient);
+            
+        }
     }
     
 }
